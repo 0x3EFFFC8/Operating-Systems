58cff3feec713a16de649ba064f3b3e419393876
0x3EFFFC8 <X3R0_SEXY@outlook.com>
2024-05-17 21:00:00 -0500

    simulator

---
+++ simulator/simulator.h
@@ -0,0 +1,10 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#define MAX_LINE_LENGTH 100 
+#define INIT_INSTRUCTIONS 4400
+
+void readASM();
+void ControlUnit(char *insU,int ins0, char *ins1, char *ins2, char *ins3, char *ins4, int *band);
+
+#endif
